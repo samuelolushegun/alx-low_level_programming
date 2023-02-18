77b555999495fa85c5f5e8f89eddb2fb30c0cf31
Samuel <etchoudinansamuel@gmail.com>
2023-02-18 13:31:46 -0800

    task 4 commitment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 {
-  char str[] ="\"Programming is like building a multilingual puzzle";
+  char str[] = "\"Programming is like building a multilingual puzzle";
+
   puts(str);
   return (0);
 }
