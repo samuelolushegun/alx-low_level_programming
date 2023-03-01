491258d0eb5572d8dcbb8939e6a125d45ea4204f
Samuel <etchoudinansamuel@gmail.com>
2023-03-01 14:12:57 -0800

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two int
+ *@x: first parameter
+ *@y: seconde parameter
+ *
+ *Return: addition of two parameters
+ */
+int add(int x, int y)
+{
+	int n = x + y;
+
+	return (n);
+}
