a8a7d1afcb162459e415ffb8b22561e893ed1004
Samuel <etchoudinansamuel@gmail.com>
2023-03-01 06:11:32 -0800

    jack bauer task :) :)

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,4 +7,45 @@
  */
 void jack_bauer(void)
 {
+	int a, b, c, d;
+
+	for (a = 48 ; a <= 50 ; a++)
+	{
+		if (a < 50)
+		{
+			for (b = 48 ; b <= 57 ; b++)
+			{
+				for (c = 48 ; c <= 53 ; c++)
+				{
+					for (d = 48 ; d <= 57 ; d++)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(58);
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+		else
+		{
+			for (b = 48 ; b <= 51 ; b++)
+			{
+				for (c = 48 ; c <= 53 ; c++)
+				{
+					for (d = 48 ; d <= 57 ; d++)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(58);
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
