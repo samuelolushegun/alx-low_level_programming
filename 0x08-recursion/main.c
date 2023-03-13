e9ec25fdf97ed3fb9aa0ed6b14ff7573a9c6b10c
Samuel <etchoudinansamuel@gmail.com>
2023-03-13 14:42:31 +0000

    task 2

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -8,19 +8,10 @@
  */
 int main(void)
 {
-	int r;
+	int n;
 
-	r = _pow_recursion(1, 10);
-	printf("%d\n", r);
-	r = _pow_recursion(1024, 0);
-	printf("%d\n", r);
-	r = _pow_recursion(2, 16);
-	printf("%d\n", r);
-	r = _pow_recursion(5, 2);
-	printf("%d\n", r);
-	r = _pow_recursion(5, -2);
-	printf("%d\n", r);
-	r = _pow_recursion(-5, 3);
-	printf("%d\n", r);
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
 	return (0);
 }
+
