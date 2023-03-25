051984360eca773c28611a11b2d3eb4fc81b736f
Samuel <etchoudinansamuel@gmail.com>
2023-03-25 01:23:40 +0000

    commit after Trying to fix some error in variadic function project

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,8 +36,8 @@ void print_all(const char * const format, ...)
 		case 's':
 			s = va_arg(ap, char *);
 			if (s == NULL)
-				printf("(nil)");
-			else
+				printf("nil");
+			if (s != NULL)
 				printf("%s", s);
 			break;
 		default:
