f2b5becb1210eb8cd066eb404a04dce1b173aa33
Samuel <etchoudinansamuel@gmail.com>
2023-06-13 17:21:08 +0000

    FIXING of task2 fail: 4th try

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,35 +13,32 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *a;
-	int i, a1, a2;
+	int i, x, a1 = 0, a2 = 0;
 
 	if (s1 == NULL)
-	{
-		s1[0] = '\0';
-		a1 = 0;
-	}
-	else
-		a1 = strlen(s1);
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2[0] = '\0';
-		a2 = 0;
-	}
-	else
-		a2 = strlen(s2);
-	a = (char *)malloc(sizeof(char) * (a1 + a2 + 1));
+		s2 = "";
+	for (i = 0; s1[i]; i++)
+		a1++;
+	a2 = strlen(s2);
+	a = (char *)malloc(sizeof(char) * (a1 + a2 + 2));
 	if (a == NULL)
 		return (NULL);
+	x = 0;
 	if (s1 != NULL)
 	{
-		for (i = 0; i < a1; i++)
-			a[i] = s1[i];
+		for (i = 0; s1[i]; i++)
+		{
+			a[x] = s1[i];
+			x++;
+		}
 	}
 	if (s2 != NULL)
 	{
-		for (i = a1; i < (a1 + a2); i++)
-			a[i] = s2[(i - a1)];
+		for (i = 0; s2[i]; i++)
+			a[x++] = s2[i];
 	}
-	a[i] = '\0';
+	a[x] = '\0';
 	return (a);
 }
