d63fa71b9980b37ba878420a43b91a52b2d0306c
Samuel <etchoudinansamuel@gmail.com>
2023-06-13 13:42:22 +0000

    task3 test 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,10 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		a[i] = (int *)malloc(sizeof(int) * width);
 	if (!a)
+	{
+		free(a);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
