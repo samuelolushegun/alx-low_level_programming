cef27f25d77bc92938ddb1b155b2e460aaa24452
Samuel <etchoudinansamuel@gmail.com>
2023-06-20 04:31:59 +0000

    commit for task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to a struct dog
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		EXIT_FAILURE;
+	if ((*d).name == NULL)
+		d->name = "(nil)";
+	printf("Name: %s\n", (*d).name);
+	if ((*d).age)
+		printf("Age: %f\n", (*d).age);
+	else
+		printf("Age: (nil)\n");
+	if ((*d).owner == NULL)
+		d->owner = "(nil)";
+	printf("Owner: %s\n", (*d).owner);
+}
