58a0ac3664d111b53162275a4ac1d7399c036bd7
Samuel <etchoudinansamuel@gmail.com>
2023-06-20 10:38:41 +0000

    commit for task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,18 +11,8 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("\n");
-	else
-	{
-		if ((*d).name == NULL)
-			d->name = "(nil)";
-		printf("Name: %s\n", (*d).name);
-		if ((*d).age)
-			printf("Age: %f\n", (*d).age);
-		else
-			printf("Age: (nil)\n");
-		if ((*d).owner == NULL)
-			d->owner = "(nil)";
-		printf("Owner: %s\n", (*d).owner);
-	}
+		EXIT_FAILURE;
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
 }
