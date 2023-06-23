c0798ba5e0184e2920146823b0b4cb295041202a
Samuel <etchoudinansamuel@gmail.com>
2023-06-23 02:25:24 +0000

    Update in header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,4 +11,6 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+void str_nil(char *s);
+
 #endif /* VARIADIC_FUNCTIONS_H */
