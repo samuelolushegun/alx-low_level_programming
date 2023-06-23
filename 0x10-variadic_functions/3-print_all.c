53e9dbbba751f8cc585dace531f41dfc5b4e1aa8
Samuel <etchoudinansamuel@gmail.com>
2023-06-23 02:05:13 +0000

    2nd trial for task 2

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,6 @@ void print_all(const char * const format, ...)
 {
 	unsigned int long i = 0;
 	va_list a;
-	char *sep = "";
 
 	va_start(a, format);
 	if (format == NULL)
@@ -24,22 +23,22 @@ void print_all(const char * const format, ...)
 		switch (*(format + i))
 		{
 		case 'c':
-			printf("%s%d", sep, va_arg(a, int));
+			printf("%d", va_arg(a, int));
 			break;
 		case 'i':
-			printf("%s%d", sep, va_arg(a, int));
+			printf("%d", va_arg(a, int));
 			break;
 		case 'f':
-			printf("%s%f", sep, va_arg(a, double));
+			printf("%f", va_arg(a, double));
 			break;
 		case 's':
 			str_nil(va_arg(a, char *));
-			printf("%s", sep);
 			break;
 		default:
 			break;
 		}
-		sep = ", ";
+		if (i < (strlen(format) - 1))
+			printf(", ");
 		i++;
 	}
 	printf("\n");
