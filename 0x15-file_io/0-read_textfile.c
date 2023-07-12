e86887a79bd2580d6a0f1c1579948d24561031ce
Samuel <etchoudinansamuel@gmail.com>
2023-07-12 00:43:26 +0000

    fix in 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include "main.h"
@@ -13,21 +14,35 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	char buffer[letters];
+	int fp;
+	char *buffer;
 	ssize_t r_letters, w_letters;
 
-	fp = fopen(filename, "r");
-	if (fp == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
+
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
 	r_letters = read(fp, buffer, letters);
 	if (r_letters == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	w_letters = write(STDOUT_FILENO, buffer, letters);
-	if(w_letters == -1 || w_letters != letters)
+	if (w_letters == -1 || (size_t)w_letters != letters)
+	{
+		free(buffer);
 		return (0);
+	}
 	close(fp);
+	free(buffer);
 	return (w_letters);
 }
