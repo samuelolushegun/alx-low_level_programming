f96557625eb78714e78cd307523ba7420206742b
Samuelolushegun <etchoudinansamuel@gmail.com>
2023-07-26 16:56:07 +0000

    Fix for task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,37 +12,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int *a, *b, r, k;
-	unsigned int i = 0, j = 0;
 	unsigned int len = ((sizeof(unsigned long int) * 8) - 1);
 
 	if (index > len)
 		return (-1);
 
-	a = malloc(sizeof(unsigned long int) * 8);
-	if (a == NULL)
-		return (-1);
-
-	while (n != 0)
-	{
-		r = n & 1;
-		n = n >> 1;
-		a[i] = r;
-		i++;
-	}
-
-	if (index > (i - 1))
-		return (-1);
-
-	b = malloc(sizeof(unsigned long int) * 8);
-	if (b == NULL)
-		return (-1);
-
-	for (k = i; k != 0; k--)
-	{
-		b[j] = a[k];
-		j++;
-	}
-
-	return (b[index]);
+	n = n >> index;
+	return (n & 1);
 }
