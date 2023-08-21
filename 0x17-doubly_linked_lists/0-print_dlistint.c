0157d591573b3d46f653778ab51e8c42d60868cc
Samuelolushegun <etchoudinansamuel@gmail.com>
2023-08-21 03:25:56 +0000

    Add Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to a dlistint_t list
+ *
+ *Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *traverse_ptr;
+	size_t compteur_de_noeud = 0;
+
+	traverse_ptr = malloc(sizeof(dlistint_t));
+	if (traverse_ptr == NULL)
+		return (EXIT_FAILURE);
+
+	traverse_ptr = h;
+
+	while (traverse_ptr != NULL)
+	{
+		printf("%d\n", traverse_ptr->n);
+		compteur_de_noeud++;
+		traverse_ptr = traverse_ptr->next;
+	}
+
+	return (compteur_de_noeud);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains Task about project:'0x17. C - Doubly linked lists'
