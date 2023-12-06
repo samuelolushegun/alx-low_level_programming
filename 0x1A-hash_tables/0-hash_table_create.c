0aae8d5d0608404ef06b5c0de3a509976ae35d46
Samuel <etchoudinansamuel@gmail.com>
2023-12-06 14:53:44 +0000

    Correction of Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	myhash_tab->size = size;
-	myhash_tab->array = malloc(size * sizeof(hash_node_t));
+	myhash_tab->array = malloc(size * sizeof(hash_node_t *));
 	if (myhash_tab->array == NULL)
 	{
 		free(myhash_tab);
