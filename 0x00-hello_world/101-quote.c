c1e9795078340bec32298810129578ccb42dca1f
Samuel <etchoudinan@gmail.com>
2025-03-21 11:35:38 +0000

    Correction in quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-  return (1);
+	char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, m, 59);
+	return (1);
 }
