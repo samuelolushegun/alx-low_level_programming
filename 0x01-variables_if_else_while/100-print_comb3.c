6cf3d163845fa3aaf6e1eceaa976c0fe3edef29f
Samuel <etchoudinan@gmail.com>
2025-03-24 21:58:18 +0000

    Commit for advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits
+ *
+ * Description: Prints unique two-digit combinations in ascending order,
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers
+ *
+ * Description: Prints unique combinations of two-digit numbers from 00 to 99
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
